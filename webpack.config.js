f2cc80906392b24e8c56fafb7934f951b8c3fdaf
Rafael Santana <rafaelsantana.devweb@gmail.com>
2024-08-05 15:32:16 -0300

    Configurações bases de css

--- webpack.config.js
+++ webpack.config.js
@@ -31,5 +31,23 @@ module.exports = {
         }
       ]
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
 }
