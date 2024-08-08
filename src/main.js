f9d7ac9ff0b0f220210d92d98e2dc4d12b21f176
Rafael Santana <rafaelsantana.devweb@gmail.com>
2024-08-08 17:31:38 -0300

    Estruturando e estilizando a sessão de progressão

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import "./styles/global.css"
 import "./styles/header.css"
 import "./styles/layout.css"
 import "./styles/loyalty-card.css"
+import "./styles/progress.css"
 import "./styles/user.css"
 
