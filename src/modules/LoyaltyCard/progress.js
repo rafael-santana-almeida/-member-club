0ad4eefea2736b6f5f9478e409fedab8b772b9b2
Rafael Santana <rafaelsantana.devweb@gmail.com>
2024-08-24 11:05:42 -0300

    Alimentando seção de progresso

---
+++ src/modules/LoyaltyCard/progress.js
@@ -0,0 +1,14 @@
+const totalCutsRemaining = document.getElementById("totalCutsRemaining")
+const totalCutsControll = document.getElementById("totalCutsControll")
+const progressBar = document.getElementById("progressBar")
+
+export function progress({ loyaltyCard }) {
+  const { totalCuts, cutsNeeded, cutsRemaining } = loyaltyCard;
+
+  totalCutsRemaining.innerText = cutsRemaining
+  totalCutsControll.innerText = `${totalCuts} de ${cutsNeeded}`
+
+  const progressAmount = (totalCuts * 100) / cutsNeeded
+
+  progressBar.style.width = `${progressAmount}%`
+}
--- src/modules/LoyaltyCard/render.js
+++ src/modules/LoyaltyCard/render.js
@@ -1,15 +1,19 @@
 import { history } from "./history"
+import { progress } from "./progress"
 
 const clientName = document.getElementById("clientName")
 const clientSince = document.getElementById("clientSince")
 const avatarImg = document.getElementById("avatarImg")
 
 export function render({ data }) {
+  const { appointmentHistory, loyaltyCard } = data
+
   avatarImg.src = data.avatarUrl
   clientName.innerText = data.name
   clientSince.innerText = `Cliente desde ${data.clientSince}`
 
-  history({ appointmentHistory: data.appointmentHistory })
+  history({ appointmentHistory })
+  progress({ loyaltyCard })
 
   console.log(data)
 }
